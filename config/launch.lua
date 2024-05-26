556819f1b151e150c2e457a0fa721879dd040625
vivy89 <1092347553@qq.com>
2024-05-26 15:05:17 +0800

    support pwsh

--- config/launch.lua
+++ config/launch.lua
@@ -6,7 +6,17 @@ local options = {
 }
 
 if platform.is_win then
-   options.default_prog = { 'powershell' }
+   options.default_prog = { 'pwsh' }
+   options.launch_menu = {
+      { label = 'PowerShell Core', args = { 'pwsh' } },
+      { label = 'PowerShell Desktop', args = { 'powershell' } },
+      { label = 'Command Prompt', args = { 'cmd' } },
+      { label = 'Nushell', args = { 'nu' } },
+      {
+         label = 'Git Bash',
+         args = { 'C:\\Users\\kevin\\scoop\\apps\\git\\current\\bin\\bash.exe' },
+      },
+   }
 elseif platform.is_mac then
    options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
    options.launch_menu = {
