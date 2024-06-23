0fbde041ef1247fc3260ab04a921ac3db54746ed
vivy89 <1092347553@qq.com>
2024-06-24 02:37:46 +0800

    bindings

--- config/bindings.lua
+++ config/bindings.lua
@@ -17,36 +17,36 @@ end
 local keys = {
    -- misc/useful --
    { key = 'F1', mods = 'NONE', action = 'ActivateCopyMode' },
-   { key = 'F2', mods = 'NONE', action = act.ActivateCommandPalette },
-   { key = 'F3', mods = 'NONE', action = act.ShowLauncher },
-   { key = 'F4', mods = 'NONE', action = act.ShowLauncherArgs({ flags = 'FUZZY|TABS' }) },
-   {
-      key = 'F5',
-      mods = 'NONE',
-      action = act.ShowLauncherArgs({ flags = 'FUZZY|WORKSPACES' }),
-   },
+   -- { key = 'F2', mods = 'NONE', action = act.ActivateCommandPalette },
+   -- { key = 'F3', mods = 'NONE', action = act.ShowLauncher },
+   -- { key = 'F4', mods = 'NONE', action = act.ShowLauncherArgs({ flags = 'FUZZY|TABS' }) },
+   -- {
+   --    key = 'F5',
+   --    mods = 'NONE',
+   --    action = act.ShowLauncherArgs({ flags = 'FUZZY|WORKSPACES' }),
+   -- },
    { key = 'F11', mods = 'NONE',    action = act.ToggleFullScreen },
-   { key = 'F12', mods = 'NONE',    action = act.ShowDebugOverlay },
-   { key = 'f',   mods = mod.SUPER, action = act.Search({ CaseInSensitiveString = '' }) },
-   {
-      key = 'u',
-      mods = mod.SUPER,
-      action = wezterm.action.QuickSelectArgs({
-         label = 'open url',
-         patterns = {
-            '\\((https?://\\S+)\\)',
-            '\\[(https?://\\S+)\\]',
-            '\\{(https?://\\S+)\\}',
-            '<(https?://\\S+)>',
-            '\\bhttps?://\\S+[)/a-zA-Z0-9-]+'
-         },
-         action = wezterm.action_callback(function(window, pane)
-            local url = window:get_selection_text_for_pane(pane)
-            wezterm.log_info('opening: ' .. url)
-            wezterm.open_with(url)
-         end),
-      }),
-   },
+   -- { key = 'F12', mods = 'NONE',    action = act.ShowDebugOverlay },
+   -- { key = 'f',   mods = mod.SUPER, action = act.Search({ CaseInSensitiveString = '' }) },
+   -- {
+   --    key = 'u',
+   --    mods = mod.SUPER,
+   --    action = wezterm.action.QuickSelectArgs({
+   --       label = 'open url',
+   --       patterns = {
+   --          '\\((https?://\\S+)\\)',
+   --          '\\[(https?://\\S+)\\]',
+   --          '\\{(https?://\\S+)\\}',
+   --          '<(https?://\\S+)>',
+   --          '\\bhttps?://\\S+[)/a-zA-Z0-9-]+'
+   --       },
+   --       action = wezterm.action_callback(function(window, pane)
+   --          local url = window:get_selection_text_for_pane(pane)
+   --          wezterm.log_info('opening: ' .. url)
+   --          wezterm.open_with(url)
+   --       end),
+   --    }),
+   -- },
 
    -- cursor movement --
    { key = 'LeftArrow',  mods = mod.SUPER,     action = act.SendString '\x1bOH' },
@@ -60,18 +60,18 @@ local keys = {
 
    -- tabs --
    -- tabs: spawn+close
-   { key = 't',          mods = 'CTRL|ALT', action = act.SpawnTab('DefaultDomain') },
-   { key = 'w',          mods = mod.SUPER,     action = act.CloseCurrentTab({ confirm = false }) },
+   { key = 't',          mods = mod.SUPER_REV, action = act.SpawnTab('DefaultDomain') },
+   { key = 'w',          mods = mod.SUPER_REV,     action = act.CloseCurrentTab({ confirm = false }) },
 
    -- tabs: navigation
    { key = 'h',          mods = mod.SUPER,     action = act.ActivateTabRelative(-1) },
    { key = 'l',          mods = mod.SUPER,     action = act.ActivateTabRelative(1) },
-   { key = 'h',          mods = mod.SUPER_REV, action = act.MoveTabRelative(-1) },
-   { key = 'l',          mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
+   { key = 'j',          mods = mod.SUPER, action = act.MoveTabRelative(-1) },
+   { key = 'k',          mods = mod.SUPER, action = act.MoveTabRelative(1) },
 
    -- window --
    -- spawn windows
-   { key = 'n',          mods = mod.SUPER,     action = act.SpawnWindow },
+   -- { key = 'n',          mods = mod.SUPER,     action = act.SpawnWindow },
 
    -- background controls --
    {
@@ -81,34 +81,34 @@ local keys = {
          backdrops:random(window)
       end),
    },
-   {
-      key = [[,]],
-      mods = mod.SUPER,
-      action = wezterm.action_callback(function(window, _pane)
-         backdrops:cycle_back(window)
-      end),
-   },
-   {
-      key = [[.]],
-      mods = mod.SUPER,
-      action = wezterm.action_callback(function(window, _pane)
-         backdrops:cycle_forward(window)
-      end),
-   },
-   {
-      key = [[/]],
-      mods = mod.SUPER_REV,
-      action = act.InputSelector({
-         title = 'Select Background',
-         choices = backdrops:choices(),
-         fuzzy = true,
-         fuzzy_description = 'Select Background: ',
-         action = wezterm.action_callback(function(window, _pane, idx)
-            ---@diagnostic disable-next-line: param-type-mismatch
-            backdrops:set_img(window, tonumber(idx))
-         end),
-      }),
-   },
+   -- {
+   --    key = [[,]],
+   --    mods = mod.SUPER,
+   --    action = wezterm.action_callback(function(window, _pane)
+   --       backdrops:cycle_back(window)
+   --    end),
+   -- },
+   -- {
+   --    key = [[.]],
+   --    mods = mod.SUPER,
+   --    action = wezterm.action_callback(function(window, _pane)
+   --       backdrops:cycle_forward(window)
+   --    end),
+   -- },
+   -- {
+   --    key = [[/]],
+   --    mods = mod.SUPER_REV,
+   --    action = act.InputSelector({
+   --       title = 'Select Background',
+   --       choices = backdrops:choices(),
+   --       fuzzy = true,
+   --       fuzzy_description = 'Select Background: ',
+   --       action = wezterm.action_callback(function(window, _pane, idx)
+   --          ---@diagnostic disable-next-line: param-type-mismatch
+   --          backdrops:set_img(window, tonumber(idx))
+   --       end),
+   --    }),
+   -- },
 
    -- panes --
    -- panes: split panes
@@ -124,19 +124,19 @@ local keys = {
    },
 
    -- panes: zoom+close pane
-   { key = 'Enter', mods = 'CTRL',     action = act.TogglePaneZoomState },
-   { key = 'w',     mods = mod.SUPER,     action = act.CloseCurrentPane({ confirm = false }) },
+   -- { key = 'Enter', mods = 'CTRL',     action = act.TogglePaneZoomState },
+   -- { key = 'w',     mods = mod.SUPER,     action = act.CloseCurrentPane({ confirm = false }) },
 
    -- panes: navigation
    { key = 'k',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Up') },
    { key = 'j',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Down') },
    { key = 'h',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Left') },
    { key = 'l',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Right') },
-   {
-      key = 'p',
-      mods = mod.SUPER_REV,
-      action = act.PaneSelect({ alphabet = '1234567890', mode = 'SwapWithActiveKeepFocus' }),
-   },
+   -- {
+   --    key = 'p',
+   --    mods = mod.SUPER_REV,
+   --    action = act.PaneSelect({ alphabet = '1234567890', mode = 'SwapWithActiveKeepFocus' }),
+   -- },
 
     -- scrollback
     { key = 'PageUp',   mods = 'SHIFT', action = act.ScrollByPage(-0.5) },
@@ -149,25 +149,25 @@ local keys = {
 
    -- key-tables --
    -- resizes fonts
-   {
-      key = 'f',
-      mods = 'LEADER',
-      action = act.ActivateKeyTable({
-         name = 'resize_font',
-         one_shot = false,
-         timemout_miliseconds = 1000,
-      }),
-   },
-   -- resize panes
-   {
-      key = 'p',
-      mods = 'LEADER',
-      action = act.ActivateKeyTable({
-         name = 'resize_pane',
-         one_shot = false,
-         timemout_miliseconds = 1000,
-      }),
-   },
+   -- {
+   --    key = 'f',
+   --    mods = 'LEADER',
+   --    action = act.ActivateKeyTable({
+   --       name = 'resize_font',
+   --       one_shot = false,
+   --       timemout_miliseconds = 1000,
+   --    }),
+   -- },
+   -- -- resize panes
+--    {
+--       key = 'p',
+--       mods = 'LEADER',
+--       action = act.ActivateKeyTable({
+--          name = 'resize_pane',
+--          one_shot = false,
+--          timemout_miliseconds = 1000,
+--       }),
+--    },
 }
 
 -- stylua: ignore
